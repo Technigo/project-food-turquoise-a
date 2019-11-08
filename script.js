bb550f65ff9602fdac10c3f7e191c629cc7be3e5
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2019-11-08 11:45:46 +0100

    Fetched sorted result

--- script.js
+++ script.js
@@ -15,44 +15,47 @@ const resultQuantity = 20;
 const theRestaurantSection = document.getElementById("restaurant-section")
 
 const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=${resultQuantity}&cuisines=${cuisineId}`
+const urlSortonPrice = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=${resultQuantity}&cuisines=${cuisineId}&sort=cost&order=asc`
+
+const restaurantSection = document.getElementById("restaurant-section")
 
 renderImage = (restaurant) => {
-    if (restaurant.photos && restaurant.photos.length > 0) {
-        return `<img src=${restaurant.photos[0].photo.thumb_url}>`
-    } else {
-        return ""
-    }
+  if (restaurant.photos && restaurant.photos.length > 0) {
+    return `<img src=${restaurant.photos[0].photo.thumb_url}>`
+  } else {
+    return `<img src="noimage.png" height=250px>`
+  }
 }
 
 rangeToDollar = (restaurant) => {
-        if (restaurant.price_range === 1 || restaurant.price_range === 2) {
-            return "$"
-        } else if (restaurant.price_range === 3 || restaurant.price_range === 4) {
-            return "$$"
-        } else if (restaurant.price_range === 5) {
-            return "$$$"
-        }
-    }
-    //filter if the restaurant has online delivery.
-    //Make it so your users can choose to only show resturants 
-    //which have delivery (has_online_delivery) or can be booked in advance (has_table_booking).
+  if (restaurant.price_range === 1 || restaurant.price_range === 2) {
+    return "$"
+  } else if (restaurant.price_range === 3 || restaurant.price_range === 4) {
+    return "$$"
+  } else if (restaurant.price_range === 5) {
+    return "$$$"
+  }
+}
+//filter if the restaurant has online delivery.
+//Make it so your users can choose to only show resturants 
+//which have delivery (has_online_delivery) or can be booked in advance (has_table_booking).
 
 showDeliveryRestaurants = (restaurant) => {
-    if (restaurant.has_online_delivery > 0) {
-        return "Has"
-    } else {
-        return "No"
-    }
+  if (restaurant.has_online_delivery > 0) {
+    return "Has"
+  } else {
+    return "No"
+  }
 }
 
 
 fetch(url, { headers: { "user-key": apiKey } })
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        json.restaurants.forEach(chineseRestaurant => {
-            // console.log(chineseRestaurant.restaurant.name);
-            document.getElementById("restaurant-section").innerHTML += ` 
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+    json.restaurants.forEach(chineseRestaurant => {
+      // console.log(chineseRestaurant.restaurant.name);
+      restaurantSection.innerHTML += ` 
   <li>${renderImage(chineseRestaurant.restaurant)}</li>  
   <li>Name: ${chineseRestaurant.restaurant.name}</li>
   <li>Avarage cost for two: ${chineseRestaurant.restaurant.average_cost_for_two} ${chineseRestaurant.restaurant.currency}</li>  
@@ -62,5 +65,26 @@ fetch(url, { headers: { "user-key": apiKey } })
   <li> ${showDeliveryRestaurants(chineseRestaurant.restaurant)} online delivery</li>
   <li> Table booking: ${chineseRestaurant.restaurant.has_table_booking}</li>
 `;
-        });
     });
+  });
+
+
+fetch(urlSortonPrice, { headers: { "user-key": apiKey } })
+  .then(response => response.json())
+  .then(json => {
+    document.getElementById("sortOnPrice").addEventListener('click', () => {
+      json.restaurants.forEach(chineseRestaurant => {
+        restaurantSection.innerHTML = ''
+        restaurantSection.innerHTML += ` 
+        <li>${renderImage(chineseRestaurant.restaurant)}</li>  
+        <li>Name: ${chineseRestaurant.restaurant.name}</li>
+        <li>Avarage cost for two: ${chineseRestaurant.restaurant.average_cost_for_two} ${chineseRestaurant.restaurant.currency}</li>  
+        <li>Address: ${chineseRestaurant.restaurant.location.address}</li>
+        <li>Rating: ${chineseRestaurant.restaurant.user_rating.aggregate_rating}/5 "${chineseRestaurant.restaurant.user_rating.rating_text}"</li>
+        <li> Avarage price-range: ${rangeToDollar(chineseRestaurant.restaurant)}</li>
+        <li> ${showDeliveryRestaurants(chineseRestaurant.restaurant)} online delivery</li>
+        <li> Table booking: ${chineseRestaurant.restaurant.has_table_booking}</li>
+      `;
+      })
+    })
+  })
